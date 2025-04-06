da31f34696876db0e31049f5a5c2eb278e420523
liah201 <rayoun@umich.edu>
2025-04-05 22:44:15 -0400

    Add files via upload

---
+++ js/effect.js
@@ -0,0 +1,47 @@
+// Fade in
+document.body.classList.add('fade-in');
+
+// fade out
+document.querySelectorAll('a').forEach(link => {
+    if (link.hostname !== window.location.hostname) return;
+
+    link.addEventListener('click', function (e) {
+        const href = link.getAttribute('href');
+        if (href && !href.startsWith('#')) {
+            e.preventDefault();
+            document.body.classList.remove('fade-in');
+            document.body.classList.add('fade-out');
+            setTimeout(() => {
+                window.location.href = href;
+            }, 400);
+        }
+    });
+});
+
+setTimeout(() => {
+    window.location.href = href;
+}, 800); // smooth transition time
+
+document.getElementById("year").textContent = new Date().getFullYear();
+
+const topBtn = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+        topBtn.classList.add("show");
+    } else {
+        topBtn.classList.remove("show");
+    }
+});
+
+// window.onscroll = () => {
+//     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+//         topBtn.style.display = "block";
+//     } else {
+//         topBtn.style.display = "none";
+//     }
+// };
+
+topBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
